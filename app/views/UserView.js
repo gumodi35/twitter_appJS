8d5c76bcae47375d0c1c523b120ae7ac61a91be1
gumodi35 <gumodi10@gmail.com>
2022-05-07 19:50:54 -0500

    iniciando con los ejercicios del sprint 3

---
+++ app/views/UserView.js
@@ -0,0 +1,20 @@
+// Agregamos el codigo para que funcione la prueba 1
+const UserService = require('./../services/UserService')
+
+// creamos la clase UserView
+class UserView {
+
+    static createUser(payload){
+        if(payload === null){
+            console.log("Error, es null")
+            return {error: "El payload no existe."}
+        } else if(typeof payload.username === 'string' && typeof payload.name === 'string' && typeof payload.id === 'number') {
+            return UserService.create(payload.id, payload.username, payload.name)
+        } else {
+            return {error: "Error, las propiedades del payload necesitan tener un valor valido"}
+        }
+    }
+}
+
+// exportamos nuestro modulo 
+module.exports = UserView
---
+++ test/views/UserViews.test.js
@@ -0,0 +1,14 @@
+// Usamos require para importar el UserView
+const UserView = require('./../../app/views/UserView')
+
+// Agregamos la prueba para el requerimiento 1 
+describe("Tests for UserView", () => {
+
+    test("Return an error object when try to create a new user whit an null payload", () => {
+        const payload = null
+        const result = UserView.createUser(payload)
+        // https://jestjs.io/docs/using-matchers#strings
+        expect(result.error).toMatch(/payload no existe/)
+    })
+
+})
